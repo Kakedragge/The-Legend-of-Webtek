3913dc97079812ab28b4e68b34f75ad94e9296bd
Kakedragge <simonhst@stud.ntnu.no>
2016-11-12 17:05:54 +0100

    Modularity requrement done

---
+++ scripts/navbar.js
@@ -0,0 +1,5 @@
+/**
+ * Created by Simon on 12.11.2016.
+ */
+
+document.write('<nav><ul id="navbar"><li><a href="biography.html">Biography</a></li><li><a href="references.html">References</a></li><li class="dropdown" onMouseEnter="show()" onMouseLeave="show()"><a href="gallery.html" class="dropbtn">Gallery</a><div class="dropdown-content" id="myDropdown"><a href="galleryn.html">Nature</a><a href="galleryp.html">Photography</a><a href="gallerypo.html">Portraits</a><a href="galleryc.html">Contemporary</a></div></li><li><a href="events.html">Events</a></li><li><a href="contact.html">Contact</a></li></ul></nav>');
