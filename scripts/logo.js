a4dfb62bcc024fa29dba59912ae84e8ac3cc7ba9
Kakedragge <simonhst@stud.ntnu.no>
2016-11-13 01:24:43 +0100

    P3 og logo.js istedet for logo

---
+++ scripts/logo.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Simon on 13.11.2016.
+ */
+
+document.write('<div id="frontpageLogo"> ' +
+    '<a href="frontpage.html"> ' +
+    '<img src="img/logo.png" id="smallLogo" alt="Logo"> ' +
+    '</a> ' +
+    '</div>');
